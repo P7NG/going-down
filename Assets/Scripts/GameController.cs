d7b6ad78f094c87d087b2786e8c536bfbc1e23f2
P7NG <egorpenkovskij273@gmail.com>
2024-10-20 13:22:42 +0700

    BeforeChangePlugins

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,14 +11,60 @@ public class GameController : MonoBehaviour
     public TakeChildrenPosition CameraFollowScript;
 
     public CarContollingScripts.CarController _carInGame;
+    public bool RandomPhase = false;
+
+    private void Start()
+    {
+        _carInGame = null;
+        Restart();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Destroy(_carInGame.gameObject);
-            GameObject car = Instantiate(Cars[CurrentCar].gameObject, SpawnPlaces[CurrentSpawnPlace].position, Quaternion.identity);
-            _carInGame = car.GetComponent<CarContollingScripts.CarController>();
-            CameraFollowScript.target = _carInGame.FollowCameraObject;
+            Restart();
         }
+    }
+
+    private void Restart()
+    {
+        if (_carInGame != null)
+        {
+            Destroy(_carInGame.gameObject);
+        }
+        GameObject car = Instantiate(Cars[CurrentCar].gameObject, SpawnPlaces[CurrentSpawnPlace].position, Quaternion.identity);
+        _carInGame = car.GetComponent<CarContollingScripts.CarController>();
+        CameraFollowScript.target = _carInGame.FollowCameraObject;
+    }
+
+    public void NextLevel()
+    {
+        if (!RandomPhase)
+        {
+            if(CurrentCar == 4)
+            {
+                CurrentCar = 0;
+
+                if(CurrentSpawnPlace == 4)
+                {
+                    RandomPhase = true;
+                }
+                else
+                {
+                    CurrentSpawnPlace++;
+                }
+            }
+            else
+            {
+                CurrentCar++;
+            }
+        }
+        else
+        {
+            CurrentSpawnPlace = Random.RandomRange(0, 4);
+            CurrentCar = Random.RandomRange(0, 4);
+        }
+
+        Restart();
     }
 }
--- Assets/Scripts/TakeChildrenPosition.cs
+++ Assets/Scripts/TakeChildrenPosition.cs
@@ -9,6 +9,7 @@ public class TakeChildrenPosition : MonoBehaviour
     public Vector3 offset = new Vector3(0, 2, -5);
     void FixedUpdate()
     {
+        if (target == null) return;
         transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * smooth);
     }
 }
