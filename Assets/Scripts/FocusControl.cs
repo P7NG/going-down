891a5953c63cd6e67929c4efe295137a0c75c932
P7NG <egorpenkovskij273@gmail.com>
2024-10-23 18:52:37 +0700

    AddFocus

---
+++ Assets/Scripts/FocusControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusControl : MonoBehaviour
+{
+    void OnApplicationFocus(bool hasFocus)
+    {
+        Silence(!hasFocus);
+    }
+
+    void OnApplicationPause(bool isPaused)
+    {
+        Silence(isPaused);
+    }
+
+    private void Silence(bool silence)
+    {
+        AudioListener.pause = silence;
+        // Or / And
+        AudioListener.volume = silence ? 0 : 1;
+    }
+}
+
